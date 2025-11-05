dc53d3b7a569002ec240f57fd9993c1a01be4260
Guilherme Henrique <guihenrique.dev@gmail.com>
2025-11-04 21:16:59 -0300

    feat: finished ui

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const imageLoader = document.getElementById('image-loader');
 const textToType = document.getElementById('text-to-type');
 const waitTime = document.getElementById('wait-time');
 const duration = document.getElementById('duration-selection');
+const clickAmount = document.getElementById('click-amount');
+const clickOptions = document.getElementById('click-options');
 const addActionButton = document.getElementById('add-action');
+const interval = document.getElementById('interval');
+const checkInterval = document.getElementById('check-interval');
 const generateJsonButton = document.getElementById('generate-json');
 const jsonResult = document.getElementById('json-result');
 const canvas = document.getElementById('canvas');
@@ -12,6 +16,7 @@ const ctx = canvas.getContext('2d');
 let actions = [];
 let currentImage = null;
 let coords = null;
+let pixel = null;
 
 imageLoader.addEventListener('change', e => {
     const reader = new FileReader();
@@ -30,12 +35,14 @@ imageLoader.addEventListener('change', e => {
 
 canvas.addEventListener('click', e => {
     if (currentImage) {
+
         const rect = canvas.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
         coords = { x, y };
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(currentImage, 0, 0);
+        pixel = ctx.getImageData(coords.x, coords.y, 1, 1).data;
         ctx.beginPath();
         ctx.arc(x, y, 5, 0, 2 * Math.PI);
         ctx.fillStyle = 'red';
@@ -44,24 +51,29 @@ canvas.addEventListener('click', e => {
 });
 
 actionType.addEventListener('change', e => {
+    imageLoader.style.display = "none";
+    clickOptions.style.display = "none";
+    waitTime.style.display = 'none';
+    textToType.style.display = 'none';
+    duration.style.display = "none";
+    clickAmount.style.display = "none";
+    interval.style.display = "none"
+    checkInterval.style.display = "none"
+
     if (e.target.value === 'TYPE') {
         textToType.style.display = 'block';
-        waitTime.style.display = 'none';
-        duration.style.display = "block";
-        imageLoader.style.display = "none";
+        interval.style.display = "block"
     } else if (e.target.value === 'WAIT_SECS') {
-        textToType.style.display = 'none';
         waitTime.style.display = 'block';
-        duration.style.display = "none";
-        imageLoader.style.display = "none";
     } else if (e.target.value === 'MOVE_TO') {
         imageLoader.style.display = "block";
         duration.style.display = "block";
+    } else if (e.target.value === 'CLICK') {
+        clickOptions.style.display = "block";
+        clickAmount.style.display = "block";
     } else {
-        imageLoader.style.display = "none";
-        textToType.style.display = 'none';
-        waitTime.style.display = 'none';
-        duration.style.display = "none";
+        imageLoader.style.display = "block";
+        checkInterval.style.display = "block"
     }
 });
 
@@ -71,7 +83,7 @@ addActionButton.addEventListener('click', () => {
         if (coords) {
             action.x = parseInt(coords.x);
             action.y = parseInt(coords.y);
-            action.duration = 1
+            action.duration = parseInt(duration.value) === 0 ? parseInt(duration.value) : 1
         } else {
             alert('Please select coordinates on the image.');
             return;
@@ -79,9 +91,26 @@ addActionButton.addEventListener('click', () => {
     }
     if (action.action === 'TYPE') {
         action.text = textToType.value;
+        action.interval = parseFloat(interval.value)
     }
-    if (action.action === 'WAIT') {
+    if (action.action === "CLICK") {
+        action.button = clickOptions.value
+        action.clicks = parseInt(clickAmount.value)
+    }
+    if (action.action === 'WAIT_SECS') {
         action.seconds = parseInt(waitTime.value);
+    }
+
+    if (action.action === "WAIT_UNTIL_COLOR") {
+        if (coords) {
+            action.color = `(${pixel[0]},${pixel[1]},${pixel[2]})`;
+            action.check_x = parseInt(coords.x);
+            action.check_y = parseInt(coords.y);
+            action.check_interval = parseFloat(checkInterval.value) === 0 ? parseFloat(checkInterval.value) : 1;
+        } else {
+            alert('Selecione um ponto na imagem para capturar a cor.');
+            return;
+        }
     }
 
     actions.push(action);
@@ -96,12 +125,12 @@ addActionButton.addEventListener('click', () => {
     waitTime.value = '';
     canvas.width = 100;
     canvas.height = 100;
-    const json = JSON.stringify(actions, null, 2);
+    const json = JSON.stringify({ actions }, null, 2);
     jsonResult.innerHTML = json
 });
 
 generateJsonButton.addEventListener('click', () => {
-    const json = JSON.stringify(actions, null, 2);
+    const json = JSON.stringify({ actions: actions }, null, 2);
     console.log(json)
     const blob = new Blob([json], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
